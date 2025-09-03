ecfc2a0c1350ff0e24c067af558b5b7ec9cb033d
The-GOAT1588 <112551075+The-GOAT1588@users.noreply.github.com>
2025-09-03 18:01:20 +0930

    Updated for generic filenames and directories (maintain same relative paths)

--- Weather2.py
+++ Weather2.py
@@ -4,9 +4,22 @@ import glob
 
 #fileList = ["stations_group_1986.csv",  "stations_group_1987.csv",  "stations_group_1988.csv",  "stations_group_1989.csv",  "stations_group_1990.csv",  "stations_group_1991.csv",  "stations_group_1992.csv",  "stations_group_1993.csv",  "stations_group_1994.csv",  "stations_group_1995.csv",  "stations_group_1996.csv",  "stations_group_1997.csv",  "stations_group_1998.csv",  "stations_group_1999.csv",  "stations_group_2000.csv",  "stations_group_2001.csv",  "stations_group_2002.csv",  "stations_group_2003.csv",  "stations_group_2004.csv",  "stations_group_2005.csv"]
 path = os.getcwd()
-csv_files = glob.glob(os.path.join(path, "C:/Users/John/Documents/Python/Assignment2/weatherData/*.csv"))
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-for f in csv_files:
+csvPath = os.path.join(script_dir, "weatherData", "*.csv")
+
+csvFiles = glob.glob(csvPath)
+
+
+print(csvFiles)
+
+def deleteFile(file_path):
+    if os.path.exists(file_path):
+        os.remove("average_temp.txt")
+    else:
+        print("deletion failed")
+
+for f in csvFiles:
     summerValues = pd.read_csv(f, usecols=['January', 'February', 'December'])
     autumnValues = pd.read_csv(f, usecols=['March', 'April', 'May'])
     winterValues = pd.read_csv(f, usecols=['June', 'July', 'August'])
@@ -17,15 +30,23 @@ for f in csv_files:
     winterAverages = winterValues.mean()
     springAverages = springValues.mean()
 
-    summerAve = round((sum(summerAverages) / 3), 1)
-    autumnAve = round((sum(autumnAverages) / 3), 1)
-    winterAve = round((sum(winterAverages) / 3), 1)
-    springAve = round((sum(springAverages) / 3), 1)
+    summerAve = str(round((sum(summerAverages) / 3), 1))
+    autumnAve = str(round((sum(autumnAverages) / 3), 1))
+    winterAve = str(round((sum(winterAverages) / 3), 1))
+    springAve = str(round((sum(springAverages) / 3), 1))
 
-print("Average for summer:",summerAve)
-
-print("Average for autumn:",autumnAve)
-
-print("Average for winter:",winterAve)
-
-print("Average for spring:",springAve)
+deleteFile("average_temp.txt")
+file = open("average_temp.txt", "a")
+file.write("Average for summer:")
+file.write(summerAve)
+file.write("°C\n") 
+file.write("Average for autumn:")
+file.write(autumnAve)
+file.write("°C\n")
+file.write("Average for winter:")
+file.write(winterAve)
+file.write("°C\n")
+file.write("Average for spring:")
+file.write(springAve)
+file.write("°C\n")
+file.close()
